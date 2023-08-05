270b40a1c63377aea418976c7b3f3bb751a7569e
Abhishek Kumar <Abhishekk55@birlasoft.com>
2023-08-05 16:05:50 +0530

    dockerfile updated

--- node-app/server.js
+++ node-app/server.js
@@ -17,7 +17,7 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
 app.use(morgan('common'));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use('/api/v1/notes', noteRoutes);
 
 
